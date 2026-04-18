952b66a1b1cb4d67c097b527536207c01d6156d1
Travis Bischel <travis.bischel+github@gmail.com>
2026-04-18 16:36:07 -0600

    remove old files

--- kcfg/setup.sh
+++
@@ -1,3 +0,0 @@
-#/kafka/bin/kafka-configs.sh --zookeeper localhost:2181 --alter --add-config 'SCRAM-SHA-256=[password=admin-secret-256],SCRAM-SHA-512=[password=admin-secret-512]' --entity-type users --entity-name adminscram
-#/kafka/bin/kafka-configs.sh --alter --add-config 'SCRAM-SHA-256=[password=admin-secret-256],SCRAM-SHA-512=[password=admin-secret-512]' --entity-type users --entity-name adminscram
-
